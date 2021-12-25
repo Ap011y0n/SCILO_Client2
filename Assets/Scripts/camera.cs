d21177c0127cf4101cf5e16685768fe01ed1f1cb
pgalmor <47526823+PoleteTomaPedete@users.noreply.github.com>
2021-12-25 17:46:44 +0100

    camera update

--- Assets/Scripts/camera.cs
+++ Assets/Scripts/camera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class camera : MonoBehaviour
 {
-    private GameObject player = null;
+    public GameObject player = null;
     // Start is called before the first frame update
     void Start()
     {
